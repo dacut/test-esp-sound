2ca58eacf5ec204663f24ad8eab1bb4c1220ef7e
David Cuthbert <dacut@kanga.org>
2023-04-21 17:18:40 -0700

    Use triangle wave instead.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![allow(dead_code)]
+
 use {
     core::time::Duration,
     esp_idf_hal::{
@@ -58,6 +60,47 @@ impl SendSinewave {
     }
 }
 
+struct SendTriangleWave {
+    buffer: Vec<u8>,
+}
+
+impl SendTriangleWave {
+    fn new(freq: f32) -> Self {
+        let buffer_size = (SAMPLE_RATE_HZ as f32 / freq) as usize;
+        let mut buffer = vec![0; buffer_size * 4];
+        let mut value: f32 = 0.0;
+        let mut value_inc = 0.1 / (buffer_size as f32);
+
+        for i in (0..buffer.len()).step_by(4) {
+            let i_value = (value * (i16::MAX as f32)) as i16 as u16;
+
+            buffer[i] = (i_value & 0x00ff) as u8;
+            buffer[i + 1] = ((i_value & 0xff00) >> 8) as u8;
+            buffer[i + 2] = (i_value & 0x00ff) as u8;
+            buffer[i + 3] = ((i_value & 0xff00) >> 8) as u8;
+            value += value_inc;
+
+            if value_inc > 0.0 && value > 1.0 {
+                value = 2.0 - value;
+                value_inc = -value_inc;
+            } else if value_inc < 0.0 && value < 1.0 {
+                value = -2.0 - value;
+                value_inc = -value_inc;
+            }
+        }
+
+        Self {
+            buffer,
+        }
+    }
+}
+
+impl SendTriangleWave {
+    pub fn send<Dir: I2sTxSupported>(&mut self, driver: &mut I2sStdDriver<'_, Dir>) -> Result<usize, EspError> {
+        driver.write(&self.buffer, TIMEOUT)
+    }
+}
+
 fn main() {
     esp_idf_sys::link_patches();
     EspLogger::initialize_default();
@@ -69,7 +112,7 @@ fn main() {
     println!("Starting application");
     let peripherals = Peripherals::take().unwrap();
     let i2s_config = I2sConfig::Config::default().dma_desc(DMA_BUFFERS as u32);
-    let clk_config = I2sConfig::StdClkConfig::from_sample_rate_hz(SAMPLE_RATE_HZ);
+    let clk_config = I2sConfig::StdClkConfig::from_sample_rate_hz(SAMPLE_RATE_HZ).clk_src(I2sConfig::ClockSource::Apll);
     let gpio_config = I2sConfig::StdGpioConfig::default();
     let slot_config = I2sConfig::StdSlotConfig::philips_slot_default(BITS_PER_SAMPLE, I2sConfig::SlotMode::Stereo);
     let std_config = I2sConfig::StdConfig::new(i2s_config, clk_config, slot_config, gpio_config);
@@ -81,13 +124,13 @@ fn main() {
     let mut i2s =
         I2sStdDriver::<I2sTx>::new_tx(peripherals.i2s0, std_config, bclk, Some(dout), AnyIOPin::none(), ws).unwrap();
 
-    let mut send_sine = SendSinewave::new(440.0);
+    let mut wave = SendTriangleWave::new(440.0);
     println!("Enabling output");
     i2s.tx_enable().unwrap();
 
     println!("Starting transmission");
 
     loop {
-        send_sine.send(&mut i2s, DMA_BUFFERS).unwrap();
+        wave.send(&mut i2s).unwrap();
     }
 }
